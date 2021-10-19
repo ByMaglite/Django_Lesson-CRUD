a7ae465ff72cc4128660a049d18231c6d96a4e38
ByMaglite <fceyhan55@gmail.com>
2021-10-19 09:20:23 +0300

    files updated

--- fscohort/urls.py
+++ fscohort/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from .views import home,student_list
+from .views import home,student_list, student_add, student_detail, student_update,student_delete
 
 urlpatterns = [
     path('', home, name="home"),
     path('student_list/', student_list, name="list"),
+    path('student_add/', student_add, name="add"),
+    path('detail/<int:id>/', student_detail, name="detail"),
+    path('update/<int:id>/', student_update, name="update"),
+    path('delete/<int:id>/', student_delete, name="delete"),
 ]   
  
--- fscohort/views.py
+++ fscohort/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
+from .forms import StudentForm
 from .models import Student
 # Create your views here.
 
@@ -18,16 +19,61 @@ def student_list(request):
     return render(request, "fscohort/student_list.html", context)
 
 def student_add(request):
-    pass
+    form = StudentForm()
+    
+    if request.method == "POST":
+        form = StudentForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+    
+    context = {
+       
+       "form":form     
+    }
+    
+    return render(request, "fscohort/student_add.html", context)
 
-def student_detail(request):
-    pass
+def student_detail(request,id):
+    student = Student.objects.get(id=id)
+    context = {
+        "student":student
+    }
+    
+    return render(request, "fscohort/student_detail.html", context)
+    
+def student_update(request, id):
+    
+    student = Student.objects.get(id=id)
+    
+    form = StudentForm(instance=student)
+    
+    if request.method == "POST":
+        form = StudentForm(request.POST, request.FILES, instance=student)
+        if form.is_valid():
+            form.save()
+            return redirect("list")
+       
+    context= {
+        
+        "student":student,
+        "form":form 
+    }
+    
+    return render(request, "fscohort/student_update.html", context)
 
-def student_update(request):
-    pass
+def student_delete(request, id):
+    
+    student = Student.objects.get(id=id)
+   
+    if request.method == "POST":
 
-def student_delete(request):
-    pass
-
+    
+        student.delete()
+        return redirect("list")
+    
+    context= {
+        "student":student
+    }
+    return render(request, "fscohort/student_delete.html",context)
 
 
